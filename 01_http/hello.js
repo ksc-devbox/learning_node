3582f7c72c4929afb16a930063f96b7b5cede72b
ksc-devbox <tjrcksm@naver.com>
2017-02-01 16:20:20 +0900

    test

--- 01_http/hello.js
+++ 01_http/hello.js
@@ -5,5 +5,5 @@ http.createServer(function (request, response){
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end("Hello World\n");
 }).listen(9995);
-console.log('commit test');
+
 console.log('sever test');
