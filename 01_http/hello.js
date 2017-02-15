dfe3403eae6d291c36aae3214c832e7f2b7eacbf
ksc-devbox <tjrcksm@naver.com>
2017-02-15 11:29:12 +0900

    study nodejs

--- 01_http/hello.js
+++ 01_http/hello.js
@@ -1,6 +1,7 @@
 /* nodejs */
 var http = require("http");
 
+//start server
 http.createServer(function (request, response){
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end("Hello World\n");
